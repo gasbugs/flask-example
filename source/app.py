a96c2f953e7dccd09c7ab359d829f28619614742
Ilsun Choi <isc0304@naver.com>
2024-05-19 18:38:53 +0900

    Update app.py

--- source/app.py
+++ source/app.py
@@ -9,6 +9,4 @@ def index():
   return render_template('index.html')
 
 if __name__=="__main__":
-  app.run(debug=True)
-  # host 등을 직접 지정하고 싶다면
-  # app.run(host="127.0.0.1", port="5000", debug=True)
+  app.run(host="0.0.0.0", port="5000", debug=True)
