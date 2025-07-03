fa44d47087a73c998f9cc92be0a478f61ae3ba2c
Ilsun Choi <isc0304@naver.com>
2025-07-04 08:03:34 +0900

    Create test_app.py

---
+++ tests/test_app.py
@@ -0,0 +1,13 @@
+# tests/test_app.py
+from source.template.app import app
+import pytest
+
+@pytest.fixture
+def client():
+    app.config['TESTING'] = True
+    with app.test_client() as client:
+        yield client
+
+def test_index_route(client):
+    response = client.get('/')
+    assert response.status_code == 200
